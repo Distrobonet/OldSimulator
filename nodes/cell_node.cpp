ddb9c403fb4640456be8e7bc5d0c5c2949fc4e23
laughingMan <isaac.hatton@gmail.com>
2012-04-20 23:58:39 +0000

    This is a helpful contribution

---
+++ nodes/cell_node.cpp
@@ -0,0 +1,8 @@
+int main()
+{
+	//ros node handle and init
+	Cell cell;
+	// initialize that shit
+	cell.spin();
+	return 0;
+}
---
+++ nodes/environment_node.cpp
@@ -0,0 +1,7 @@
+int main()
+{
+	// initialize ROS stuff
+	Environment env(n_cells);
+	env.spin();
+	return 0;
+}
