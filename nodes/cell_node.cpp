f5fe245eb7dd6eb9cb5f4a4f5118eb499c7a104f
breakspirit <breakspirit@gmail.com>
2012-04-21 06:45:56 +0000

    made cell an object instead of a pointer

--- nodes/cell_node.cpp
+++ nodes/cell_node.cpp
@@ -4,13 +4,13 @@ int main(bool doSpin)
 {
 	//TODO: this needs to be finished
 	//ros node handle and init
-	Cell *c = new Cell;
+	Cell thisCell;
 
 	// TODO: initialize x & y from the "base_pose_ground_truth"?
-	c->x = 0.0f;
-	c->y = 0.0f;
-	c->setHeading(90.0f);	// default heading
+	thisCell.x = 0.0f;
+	thisCell.y = 0.0f;
+	thisCell.setHeading(90.0f);	// default heading
 
-	c->update(doSpin);
+	thisCell.update(doSpin);
 	return 0;
 }
